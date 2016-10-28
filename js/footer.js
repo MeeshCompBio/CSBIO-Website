e7e4863ab64051193a1f2a420e963833c60d1af7
Jean-Michel "Meesh <mich0391@umn.edu>
2016-10-28 16:21:05 -0500

    Website initialization
    Uploading all the code I had on my workstation

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+//This is the header for every web page so we don't have to copy paste it
+document.write(
+"        <footer>" +
+"           <div class='row'>" +
+"                <div class='col-lg-12'>" +
+"                    <p>Copyright &copy; CSBIOLAB</p>" +
+"                    <p><small>Website made by Jean-Michel Michno (Meesh)  (mich0391@umn.edu)</small></p>" +
+"                </div>" +
+"            </div>" +
+"        </footer>"
+)
